adf9049a1ff3f3bf795109cf8dc8431c91be2c35
Isis Ebsen <root@5ht2.me>
2023-10-04 15:08:49 +0530

    add JSDoc to script file

--- static/script.mjs
+++ static/script.mjs
@@ -6,18 +6,43 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+/**
+ * @typedef {Object} Stats
+ * @property {number} num_visits
+ * @property {number} total_beats
+ * @property {number} uptime
+ * @property {number} last_seen
+ * @property {number} last_seen_relative
+ */
+
 const $i = document.getElementById.bind(document);
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October','November','December'];
 const units = {Y: 'year', m: 'month', w: 'week', d: 'day', H: 'hour', M: 'minute', S: 'second'};
 
+/**
+ * Pad a number with a leading zero if it is less than 10.
+ * @param {number} n The number to pad. 
+ * @returns {string} The padded number.
+ */
 function zeroPad(n) {
   return n < 10 ? '0' + n : n;
 }
 
+/**
+ * Pluralize a string based on a number.
+ * @param {number} n The number of items.
+ * @param {string} s The singular form of the item.
+ * @returns {string}
+ */
 function plural(n, s) {
   return `${n} ${s}${n === 1 ? '' : 's'}`;
 }
 
+/**
+ * Format a Unix timestamp as a human-readable date.
+ * @param {number} secs A Unix timestamp.
+ * @returns {string} The formatted date.
+ */
 function formatDate(secs) {
   const dt = new Date(secs * 1000);
   const Y = dt.getUTCFullYear();
@@ -29,6 +54,11 @@ function formatDate(secs) {
   return `${d} ${m} ${Y} ${H}:${M}:${S} UTC`;
 }
 
+/**
+ * Format `secs` seconds as a human-readable duration.
+ * @param {number} secs The number of seconds in the duration.
+ * @returns {string} The formatted duration.
+ */
 function formatRelativeTime(secs) {
   let Y, m, w, d, H, M, S, rem;
   [Y, rem] = [Math.floor(secs / 31536000), secs % 31536000];
@@ -52,6 +82,12 @@ function formatRelativeTime(secs) {
   return arr.slice(0, -1).join(', ') + ', and ' + arr.slice(-1);
 }
 
+/**
+ * Refresh components on the stats page.
+ * This is a lightweight and more retro version of what Preact does.
+ * @param {Stats} stats
+ * @returns {void} 
+ */
 function Stats(stats) {
   $i('visits').innerText = stats.num_visits.toLocaleString('en-GB');
   $i('devices').innerText = stats.devices.length.toLocaleString('en-GB');
@@ -59,6 +95,12 @@ function Stats(stats) {
   $i('uptime').innerText = formatRelativeTime(stats.uptime);
 }
 
+/**
+ * Refresh components on the index page.
+ * This is a lightweight and more retro version of what Preact does.
+ * @param {Stats} stats
+ * @returns {void} 
+ */
 function Index(stats) {
   $i('last-seen').innerText = formatDate(stats.last_seen);
   $i('time-difference').innerText = formatRelativeTime(stats.last_seen_relative, true);
@@ -76,6 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ws = new WebSocket(url.href);
 
   ws.onmessage = (ev) => {
+    /** @type {Stats} */
     let json = JSON.parse(ev.data);
     component(json);
   };
