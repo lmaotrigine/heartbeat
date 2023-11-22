649bba071266b4394340217100dd2ffee1bcadd9
Isis Ebsen <root@5ht2.me>
2023-11-22 14:39:39 +0530

    switch to common build utils for build metadata

--- build.rs
+++ build.rs
@@ -14,59 +14,8 @@
 )]
 
 fn main() {
-    let mut version = env!("CARGO_PKG_VERSION").to_owned();
-    let rev = git_revision().unwrap_or_else(|| "main".into());
-    if ["a", "b", "rc"].iter().any(|s| version.ends_with(s)) {
-        if let Some(count) = git_commit_count() {
-            version.push('.');
-            version.push_str(&count);
-        }
-        version.push_str(&format!("+g{rev}"));
-    }
+    let version = heartbeat_sys::build::long_version(env!("CARGO_PKG_VERSION"));
+    let rev = heartbeat_sys::build::git_revision().unwrap_or_else(|| "main".into());
     println!("cargo:rustc-env=HB_VERSION={version}");
     println!("cargo:rustc-env=HB_GIT_REVISION={rev}");
-}
-
-fn git_revision() -> Option<String> {
-    std::process::Command::new("git")
-        .args(["describe", "--tags", "--exact-match"])
-        .output()
-        .ok()
-        .and_then(|output| {
-            let v = String::from_utf8_lossy(&output.stdout).trim().to_string();
-            if v.is_empty() {
-                None
-            } else {
-                Some(v)
-            }
-        })
-        .or_else(|| {
-            std::process::Command::new("git")
-                .args(["rev-parse", "--short", "HEAD"])
-                .output()
-                .ok()
-                .and_then(|output| {
-                    let v = String::from_utf8_lossy(&output.stdout).trim().to_string();
-                    if v.is_empty() {
-                        None
-                    } else {
-                        Some(v)
-                    }
-                })
-        })
-}
-
-fn git_commit_count() -> Option<String> {
-    std::process::Command::new("git")
-        .args(["rev-list", "--count", "HEAD"])
-        .output()
-        .ok()
-        .and_then(|output| {
-            let v = String::from_utf8_lossy(&output.stdout).trim().to_string();
-            if v.is_empty() {
-                None
-            } else {
-                Some(v)
-            }
-        })
 }
